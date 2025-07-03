996a91e51a56dbef4746920bb10230620553f0bf
Xinyu <isxinyuliu@gmail.com>
2025-07-03 15:22:46 +0800

    Update battery.py

--- scripts/Android_devices/battery_test/battery.py
+++ scripts/Android_devices/battery_test/battery.py
@@ -5,11 +5,11 @@ prompt = 'Plan a 7-day itinerary for two people traveling from Austria to KAUST 
 
 
 def create_folder(device, model):
-    folder_path = f"/Users/xinyu/Documents/chatterbox/battery/{device}/{model}"
+    folder_path = f"/Users/Documents/chatterbox/battery/{device}/{model}"
 
     # check if folder exists
     if not os.path.exists(folder_path):
-        # 如果目录不存在，则创建目录
+        # if the folder doesn't exist, create one
         os.makedirs(folder_path)
         print(f"Folder {folder_path} created.")
     else:
@@ -18,7 +18,7 @@ def create_folder(device, model):
 def execute_commands(loop_command, number_of_runs, output_file):
     # run a python terminal
     process = subprocess.Popen(
-        ["python", "-i"],  # 启动 Python 交互式终端
+        ["python", "-i"],  # start terminal
         stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, errors='replace'
     )
 
@@ -49,7 +49,7 @@ def delete(output_file):
 
 
 def test(device, number, model, stop_event):
-    output1 = f"/Users/xinyu/Documents/chatterbox/battery/{device}/{model}/cli-long_prompt-n1024.txt"
+    output1 = f"/Users/Documents/chatterbox/battery/{device}/{model}/cli-long_prompt-n1024.txt"
     time1 = 1
     command1 = f"adb -s {number} shell \"cd /data/local/tmp/adb-cpu/mcpu-cortex-x1/bin && export LD_LIBRARY_PATH=/data/local/tmp/adb-cpu/mcpu-cortex-x1/lib:$LD_LIBRARY_PATH && ./llama-cli -m /data/local/tmp/models/{model} \
                 -p {prompt} \
