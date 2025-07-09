dc846335e62cb84feff24672558edae364913d63
Xinyu <isxinyuliu@gmail.com>
2025-07-09 18:42:32 +0800

    Update test_consistency_speed.py

--- scripts/Android_devices/speed_and_consistency_test/test_consistency_speed.py
+++ scripts/Android_devices/speed_and_consistency_test/test_consistency_speed.py
@@ -15,6 +15,13 @@ bin_dir = "/data/local/tmp/adb-cpu/mcpu-cortex-x1/bin"
 lib_dir = "/data/local/tmp/adb-cpu/mcpu-cortex-x1/lib"
 model_dir = "/data/local/tmp/adb-cpu/model"
 
+# switch of tests
+TEST_CONS = True
+TEST_PP = True
+TEST_TG = True
+TEST_BT = True
+TEST_TT = True
+
 def create_folder(device, model):
     folder_path = os.path.join(output_dir, device, model)
     print(folder_path)
@@ -100,23 +107,34 @@ def test(device, number, model):
     time5 = 1
     command5 = f"adb -s {number} shell \"cd {bin_dir} && export LD_LIBRARY_PATH={lib_dir}:$LD_LIBRARY_PATH && ./llama-bench -m {model_dir}/{model} -n 0 -n 16 -p 64 -t 1,2,4,8,16,32 -o json\""
 
-    delete(output0)
-    delete(output1)
-    delete(output2)
-    delete(output3)
-    delete(output4)
-    delete(output5)
-    execute_commands(command0, time0, output0)
-    sleep(60 * 10)  # 10 min, for cooling down
-    execute_commands(command1, time1, output1)
-    sleep(60 * 10)
-    execute_commands(command2, time2, output2)
-    sleep(60 * 10)
-    execute_commands(command3, time3, output3)
-    sleep(60 * 10)
-    execute_commands(command4, time4, output4)
-    sleep(60 * 10)
-    execute_commands(command5, time5, output5)
+    if TEST_CONS:
+        delete(output0)
+        execute_commands(command0, time0, output0)
+        sleep(60 * 10)  # 10 min, for cooling down
+        delete(output1)
+        execute_commands(command1, time1, output1)
+        sleep(60 * 10)
+
+    if TEST_PP:
+        delete(output2)
+        execute_commands(command2, time2, output2)
+        sleep(60 * 10)
+
+    if TEST_TG:
+        delete(output3)
+        execute_commands(command3, time3, output3)
+        sleep(60 * 10)
+
+    if TEST_BT:
+        delete(output4)
+        execute_commands(command4, time4, output4)
+        sleep(60 * 10)
+
+
+    if TEST_TT:
+        delete(output5)
+        execute_commands(command5, time5, output5)
+        sleep(60 * 10)
 
 if __name__ == "__main__":
     # test("ml2", MAGICLEAP, "Meta-Llama-3.1-8B-Instruct-IQ2_M.gguf")
